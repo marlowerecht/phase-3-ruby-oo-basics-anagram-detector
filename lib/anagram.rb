9f02f60246918c628bbb5eec8e1bfa36464d5554
Marlowe Recht <recht.marlowe@gmail.com>
2023-01-30 22:50:23 +0100

    commit tree 461ff06d5de65cb3984775d08f1d23e241a8fe46

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match potential_anagrams
+        sorted_array_of_letters = self.word.chars.sort
+        sorted_array_of_potential_anagrams = potential_anagrams.map { |anagram| anagram.chars.sort }
+        checking_for_matches = sorted_array_of_potential_anagrams.filter { |anagram| anagram == sorted_array_of_letters }
+        if checking_for_matches == []
+            return []
+        else
+            these_match = []
+            checking_for_matches.map { |anagram| anagram.join }
+            potential_anagrams.filter do |anagram|
+                if checking_for_matches.include?(anagram.chars.sort)
+                    these_match << anagram
+                end
+            end
+        end
+    end
+
+end
+
+hello = Anagram.new("hello")
+# binding.pry
